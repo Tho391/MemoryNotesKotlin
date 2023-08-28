917ac60966fa0f915477a6879e605e87826f0902
Nguyễn Quang Thọ <quangthonguyen391@gmail.com>
2023-08-28 14:33:30 +0700

    refactor listFragment

--- app/src/main/java/com/devtides/memorynotes/presentation/ListFragment.kt
+++ app/src/main/java/com/devtides/memorynotes/presentation/ListFragment.kt
@@ -2,52 +2,51 @@ package com.devtides.memorynotes.presentation
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
-
-import com.devtides.memorynotes.R
+import com.devtides.memorynotes.databinding.FragmentListBinding
 import com.devtides.memorynotes.framework.ListViewModel
-import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment(), ListAction {
 
+    private lateinit var binding: FragmentListBinding
     private val notesListAdapter = NotesListAdapter(arrayListOf(), this)
     private lateinit var viewModel: ListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        binding = FragmentListBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        notesListView.apply {
+        binding.notesListView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = notesListAdapter
         }
 
-        addNote.setOnClickListener { goToNoteDetails() }
+        binding.addNote.setOnClickListener { goToNoteDetails() }
 
-        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel = ViewModelProviders.of(this)[ListViewModel::class.java]
 
         observeViewModel()
     }
 
-    fun observeViewModel() {
-        viewModel.notes.observe(this, Observer {notesList ->
-            loadingView.visibility = View.GONE
-            notesListView.visibility = View.VISIBLE
+    private fun observeViewModel() {
+        viewModel.notes.observe(viewLifecycleOwner) { notesList ->
+            binding.loadingView.visibility = View.GONE
+            binding.notesListView.visibility = View.VISIBLE
             notesListAdapter.updateNotes(notesList.sortedByDescending { it.updateTime })
-        })
+        }
     }
 
     override fun onResume() {
@@ -57,7 +56,7 @@ class ListFragment : Fragment(), ListAction {
 
     private fun goToNoteDetails(id: Long = 0L) {
         val action = ListFragmentDirections.actionGoToNote(id)
-        Navigation.findNavController(notesListView).navigate(action)
+        Navigation.findNavController(binding.notesListView).navigate(action)
     }
 
     override fun onClick(id: Long) {
--- core/src/main/java/com/devtides/core/usecase/GetWordCount.kt
+++ core/src/main/java/com/devtides/core/usecase/GetWordCount.kt
@@ -6,9 +6,7 @@ class GetWordCount {
     operator fun invoke(note: Note) = getCount(note.title) + getCount(note.content)
 
     private fun getCount(str: String) =
-        str.split(" ", "\n")
-            .filter {
-                it.contains(Regex(".*[a-zA-Z].*"))
-            }
-            .count()
+        str.split(" ", "\n").count {
+            it.contains(Regex(".*[a-zA-Z].*"))
+        }
 }
